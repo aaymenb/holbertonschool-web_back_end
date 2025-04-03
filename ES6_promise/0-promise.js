922d9daf88340e9daa7534702ae80a4d1c65e0a1
aaymenb <9954@holbertonstudents.com>
2025-04-04 00:15:46 +0200

    .

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,3 +1,14 @@
-export default function getResponseFromAPI() {
-	return Promise.resolve(1);
+// Only make Promise
+function getResponseFromAPI() {
+	return new Promise((resolve, reject) => {
+		/* eslint-disable */
+		if (true) {
+			resolve();
+		} else {
+			reject();
+		}
+		/* eslint-enable */
+	});
 }
+
+export default getResponseFromAPI;
