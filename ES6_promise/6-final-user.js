85758cc05c74fa1c9718897bfc1eeca95922b782
aaymenb <9954@holbertonstudents.com>
2025-04-07 11:38:24 +0200

    .

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,14 +1,16 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) =>
-    results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : String(result.reason),
-    }))
-  );
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+	const userData = await signUpUser(firstName, lastName).then((data) => ({
+		status: 'fulfilled',
+		value: data,
+	}));
+
+	const fileData = await uploadPhoto(fileName).catch((err) => ({
+		status: 'rejected',
+		value: err.toString(),
+	}));
+
+	return Promise.resolve([userData, fileData]);
 }
