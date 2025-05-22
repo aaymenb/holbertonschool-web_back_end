0ac95fde7ee97509c607068404290e1ff533be8c
AymenB <aymen1996ben@gmail.com>
2025-05-22 13:55:09 +0200

    feat: Add complex HTTP server using Express

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,38 @@
+import { readDatabase } from '../utils.js';
+
+export class StudentsController {
+  static async getAllStudents(req, res) {
+    try {
+      const students = await readDatabase(process.argv[2]);
+      const fields = Object.keys(students).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+      
+      let response = 'This is the list of our students\n';
+      fields.forEach(field => {
+        const count = students[field].length;
+        const list = students[field].join(', ');
+        response += `Number of students in ${field}: ${count}. List: ${list}\n`;
+      });
+      
+      res.status(200).send(response);
+    } catch (error) {
+      res.status(500).send('Cannot load the database');
+    }
+  }
+
+  static async getAllStudentsByMajor(req, res) {
+    const { major } = req.params;
+    
+    if (major !== 'CS' && major !== 'SWE') {
+      res.status(500).send('Major parameter must be CS or SWE');
+      return;
+    }
+
+    try {
+      const students = await readDatabase(process.argv[2]);
+      const list = students[major].join(', ');
+      res.status(200).send(`List: ${list}`);
+    } catch (error) {
+      res.status(500).send('Cannot load the database');
+    }
+  }
+} 
---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,26 @@
+import fs from 'fs';
+
+export const readDatabase = (filePath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.trim().split('\n');
+      const students = {};
+      
+      // Skip header line
+      for (let i = 1; i < lines.length; i++) {
+        const [firstname, , , field] = lines[i].split(',');
+        if (!students[field]) {
+          students[field] = [];
+        }
+        students[field].push(firstname);
+      }
+
+      resolve(students);
+    });
+  });
+}; 
