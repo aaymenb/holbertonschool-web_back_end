4dd7eb7c86a674df91939c07895f807de64ddc46
aaymenb <aymen1996ben@gmail.com>
2025-05-22 13:11:12 +0200

    NodeJS

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -4,23 +4,22 @@ const countStudents = require('./3-read_file_async');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-  const databasePath = process.argv[2];
+  const database = process.argv[2];
+  res.write('This is the list of our students\n');
 
-  countStudents(databasePath)
-    .then(() => {
-      res.status(200).send('This is the list of our students');
+  countStudents(database)
+    .then((output) => {
+      res.end(output);
     })
-    .catch((error) => {
-      res.status(500).send(error.message);
+    .catch((err) => {
+      res.end(err.message);
     });
 });
 
-app.listen(1245, () => {
-  console.log('Server is running on port 1245');
-});
+app.listen(1245);
 
 module.exports = app;
