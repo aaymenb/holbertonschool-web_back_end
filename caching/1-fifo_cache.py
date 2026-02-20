85f23748d1a99a9c43168e6116349012a3a83990
aaymenb <aymen1996ben@gmail.com>
2026-02-20 22:06:58 +0100

    test

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -4,31 +4,32 @@ from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """ Système de mise en cache FIFO """
+    """ FIFOCache defines a FIFO caching system """
 
     def __init__(self):
-        """ Initialisation """
+        """ Initialize """
         super().__init__()
-        self.order = []  # Liste pour suivre l'ordre d'entrée exact
+        self.keys_order = []
 
     def put(self, key, item):
-        """ Ajoute un élément au cache """
+        """ Add an item in the cache """
         if key is None or item is None:
             return
 
-        # Si la clé n'est pas déjà dans le cache, on l'ajoute à la file
+        # Si la clé est déjà là, on ne l'ajoute pas à la liste d'ordre
+        # (elle garde sa place de "vieille" clé)
         if key not in self.cache_data:
-            self.order.append(key)
+            self.keys_order.append(key)
         
         self.cache_data[key] = item
 
-        # Si on dépasse la limite
+        # On vérifie la limite APRÈS l'ajout
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            # On récupère le plus ancien dans la liste 'order'
-            discarded_key = self.order.pop(0)
-            del self.cache_data[discarded_key]
-            print("DISCARD: {}".format(discarded_key))
+            # On retire le premier de la liste (le plus ancien)
+            first_key = self.keys_order.pop(0)
+            del self.cache_data[first_key]
+            print("DISCARD: {}".format(first_key))
 
     def get(self, key):
-        """ Récupère un élément du cache """
+        """ Get an item by key """
         return self.cache_data.get(key)
