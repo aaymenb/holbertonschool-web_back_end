7f52a3146076e3b93966cda7dfcc9502efb85ce5
aaymenb <aymen1996ben@gmail.com>
2026-02-20 22:04:07 +0100

    test

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,40 +1,72 @@
 #!/usr/bin/python3
-""" FIFO caching module
 """
+    BaseCache module
+"""
+
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """ FIFOCache defines a FIFO caching system
+    """ FIFOCache define a FIFO algorithm to use cache
+
+      To use:
+          >>> my_cache = BasicCache()
+      >>> my_cache.print_cache()
+      Current cache:
+
+          >>> my_cache.put("A", "Hello")
+      >>> my_cache.print_cache()
+      A: Hello
+
+      >>> print(my_cache.get("A"))
+      Hello
+
+      Ex:
+          >>> print(self.cache_data)
+      {A: "Hello", B: "World", C: "Holberton", D: "School"}
+      >>> my_cache.put("C", "Street")
+      >>> print(self.cache_data)
+      {A: "Hello", B: "World", C: "Street", D: "School"}
+
+      >>> my_cache.put("F", "COD")
+      DISCARD: A
+      >>> print(self.cache_data)
+      {F: "COD", B: "World", C: "Holberton", D: "School"}
     """
 
     def __init__(self):
-        """ Initialize
+        """ Initiliaze
         """
         super().__init__()
-        self.keys_order = []
 
     def put(self, key, item):
-        """ Add an item in the cache
         """
-        if key is None or item is None:
-            return
+            modify cache data
 
-        # Si la clé est NOUVELLE, on l'ajoute à la liste de suivi
-        if key not in self.cache_data:
-            self.keys_order.append(key)
-        
-        # On met à jour ou on ajoute dans le dictionnaire
-        self.cache_data[key] = item
+            Args:
+                key: of the dict
+                item: value of the key
+        """
+        if key or item is not None:
+            valuecache = self.get(key)
+            if valuecache is None:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    keydel = list(self.cache_data.keys())[0]
+                    del self.cache_data[keydel]
+                    print("DISCARD: {}".format(keydel))
 
-        # On vérifie si on dépasse la limite
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            # On prend le premier élément de la liste (le plus vieux)
-            discarded_key = self.keys_order.pop(0)
-            del self.cache_data[discarded_key]
-            print("DISCARD: {}".format(discarded_key))
+            self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key
         """
-        return self.cache_data.get(key)
+            modify cache data
+
+            Args:
+                key: of the dict
+
+            Return:
+                value of the key
+        """
+
+        valuecache = self.cache_data.get(key)
+        return valuecache
