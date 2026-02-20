7511bb11af267a1d55bc230f93f3e64c7f2630ad
aaymenb <aymen1996ben@gmail.com>
2026-02-20 22:02:52 +0100

    test

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,20 +1,40 @@
-def put(self, key, item):
-    """ Méthode put corrigée pour le checker """
-    if key is None or item is None:
-        return
+#!/usr/bin/python3
+""" FIFO caching module
+"""
+from base_caching import BaseCaching
 
-    # Si la clé est déjà là, on la supprime de la liste d'ordre 
-    # pour la remettre à la fin (ou on ne touche pas à l'ordre, 
-    # selon l'interprétation stricte du FIFO)
-    if key in self.cache_data:
-        self.keys_order.remove(key)
 
-    self.cache_data[key] = item
-    self.keys_order.append(key)
+class FIFOCache(BaseCaching):
+    """ FIFOCache defines a FIFO caching system
+    """
 
-    # On vérifie si on dépasse la limite
-    if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-        # On récupère le premier élément (le plus vieux)
-        discarded_key = self.keys_order.pop(0)
-        del self.cache_data[discarded_key]
-        print("DISCARD: {}".format(discarded_key))
+    def __init__(self):
+        """ Initialize
+        """
+        super().__init__()
+        self.keys_order = []
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+
+        # Si la clé est NOUVELLE, on l'ajoute à la liste de suivi
+        if key not in self.cache_data:
+            self.keys_order.append(key)
+        
+        # On met à jour ou on ajoute dans le dictionnaire
+        self.cache_data[key] = item
+
+        # On vérifie si on dépasse la limite
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            # On prend le premier élément de la liste (le plus vieux)
+            discarded_key = self.keys_order.pop(0)
+            del self.cache_data[discarded_key]
+            print("DISCARD: {}".format(discarded_key))
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        return self.cache_data.get(key)
