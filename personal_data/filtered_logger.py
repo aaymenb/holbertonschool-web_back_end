aedd8b5f32725f706ad3265c02c821664f12b93e
aaymenb <aymen1996ben@gmail.com>
2026-02-26 16:20:31 +0100

    done

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,6 +1,5 @@
 import re
 
 def filter_datum(fields, redaction, message, separator):
-    """ Obfuscates specific fields in a log message using regex """
-    pattern = '|'.join([f"{f}=.*?{separator}" for f in fields])
-    return re.sub(pattern, lambda m: f"{m.group().split('=')[0]}={redaction}{separator}", message)
+    """ Obfuscates specific fields in a log message using a single regex """
+    return re.sub(f"({'|'.join(fields)})=.*?{separator}", f"\\1={redaction}{separator}", message)
