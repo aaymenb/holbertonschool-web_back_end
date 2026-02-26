2c051ccf73c8db32427c9678ebce8bc7e91ad842
aaymenb <aymen1996ben@gmail.com>
2026-02-26 16:28:22 +0100

    done

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Hash_password Module
+"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ Function that hashes a password with a salt.
+    """
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Function that checks if a password matches its hashed version.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
