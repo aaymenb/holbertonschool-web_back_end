7b33df090a232863bfaf0d27d9af2c7062f707e6
AymenB <aymen1996ben@gmail.com>
2026-02-27 22:20:58 +0100

    test

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Basic Auth class
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic authentication class - inherits from Auth"""
+    pass
