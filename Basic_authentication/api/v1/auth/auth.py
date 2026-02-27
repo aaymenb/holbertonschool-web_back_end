c70e1a5fda370f2204293c2cefe3511e21058c61
AymenB <aymen1996ben@gmail.com>
2026-02-27 22:16:10 +0100

    test

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -12,8 +12,17 @@ class Auth:
     """Template for all authentication systems"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Returns False - path and excluded_paths will be used later"""
-        return False
+        """Returns True if path is not in excluded_paths, False otherwise.
+        Slash tolerant: /api/v1/status and /api/v1/status/ match /api/v1/status/
+        """
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        path_normalized = path if path.endswith('/') else path + '/'
+        if path_normalized in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Returns None - request will be the Flask request object"""
