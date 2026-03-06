ca628f29d7fb3ef92427ba364092e85aa5c7852a
AymenB <aymen1996ben@gmail.com>
2026-03-06 23:03:51 +0100

    test

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -31,12 +31,11 @@ def before_request():
         return
     request.current_user = auth.current_user(request)
     exempt_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                    '/api/v1/forbidden/', '/api/v1/auth_session/login/',
-                    '/api/v1/auth_session/login']
+                    '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, exempt_paths):
         return
     if auth.authorization_header(request) is None and \
-       auth.session_cookie(request) is None:
+            auth.session_cookie(request) is None:
         abort(401)
     if request.current_user is None:
         abort(403)
