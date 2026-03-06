b3556d4117c0879baee3bb4079cf4456d32f05dc
AymenB <aymen1996ben@gmail.com>
2026-03-06 22:43:59 +0100

    test

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Session authentication class
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Empty class for session authentication - inherits from Auth"""
+    pass
