2f5e754bd76507644481ddeaf5a72d4dd78db238
AymenB <aymen1996ben@gmail.com>
2026-03-06 23:12:58 +0100

    test

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -42,3 +42,16 @@ class SessionAuth(Auth):
         if not users:
             return None
         return users[0]
+
+    def destroy_session(self, request=None):
+        """Deletes the user session / logout"""
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True
--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -3,7 +3,7 @@
 Session authentication views
 """
 from os import getenv
-from flask import jsonify, request, make_response
+from flask import jsonify, request, make_response, abort
 from api.v1.views import app_views
 from models.user import User
 
@@ -31,3 +31,14 @@ def login():
     session_name = getenv("SESSION_NAME", "_my_session_id")
     response.set_cookie(session_name, session_id, path='/')
     return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """DELETE /api/v1/auth_session/logout
+    Handle session logout
+    """
+    from api.v1.app import auth
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify({}), 200
