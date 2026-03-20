3383c95cf28efb4e135ffc18164129a93986e69d
aaymenb <aymen1996ben@gmail.com>
2026-03-20 21:40:11 +0100

    task0

--- user.py
+++ user.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python3
-""" User database """
-from sqlalchemy.ext.declarative import declarative_base
+"""
+User model for SQLAlchemy mapping.
+Represents the 'users' table in the database.
+"""
+
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class User(Base):
-    """ User Initial Class """
+
+    """
+    SQLAlchemy User model for 'users' table.
+    Stores user authentication and session details.
+    """
+
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
-    email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    id: int = Column(Integer, primary_key=True)
+    email: str = Column(String(250), nullable=False)
+    hashed_password: str = Column(String(250), nullable=False)
+    session_id: str = Column(String(250), nullable=True)
+    reset_token: str = Column(String(250), nullable=True)
