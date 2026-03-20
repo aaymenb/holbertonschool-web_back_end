606dd06853c1dc520acf10ca2232ec62db1aa8e1
aaymenb <aymen1996ben@gmail.com>
2026-03-20 21:53:29 +0100

    task0

--- user.py
+++ user.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3
 """
-Module pour le modèle User
+Module for User model
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-# 1. Création de la base déclarative comme expliqué dans votre texte
 Base = declarative_base()
 
-# 2. Définition de la classe User mappée à la table 'users'
+
 class User(Base):
     """
-    Modèle SQLAlchemy pour la table 'users'
+    User class for the 'users' table
     """
     __tablename__ = 'users'
 
-    # Attributs demandés par l'exercice (et non par le tutoriel générique)
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
