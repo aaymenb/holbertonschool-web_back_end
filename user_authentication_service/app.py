5f49739f9b4aa4f0d4fab8a9acc132204d5bc6a0
aaymenb <aymen1996ben@gmail.com>
2026-03-20 23:12:39 +0100

    done

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python3
 """
-Basic Flask app module
+Flask app module with user registration
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def welcome() -> str:
     """
     GET /
-    Return a welcome message in JSON format.
+    Return a welcome message.
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """
+    POST /users
+    Registers a new user or returns an error if the email exists.
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
