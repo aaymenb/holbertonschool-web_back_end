fa9daec19106a81ab9fe0b054d9a2caa34ec81c6
aaymenb <aymen1996ben@gmail.com>
2026-03-20 21:55:58 +0100

    task0

--- user.py
+++ user.py
@@ -1,21 +1,51 @@
 #!/usr/bin/env python3
 """
-Module for User model
+DB module
 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from user import Base, User  # Assurez-vous d'importer User ici
 
-Base = declarative_base()
 
-
-class User(Base):
+class DB:
     """
-    User class for the 'users' table
+    DB class to interact with the database
     """
-    __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
-    email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    def __init__(self) -> None:
+        """
+        Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """
+        Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        Crée et enregistre un nouvel utilisateur dans la base de données.
+        
+        Args:
+            email (str): L'email de l'utilisateur.
+            hashed_password (str): Le mot de passe haché.
+            
+        Returns:
+            User: L'objet utilisateur créé.
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        self._session.refresh(new_user)
+        return new_user
