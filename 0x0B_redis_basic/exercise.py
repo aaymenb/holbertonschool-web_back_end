812750ea6f70ccecb9826ce66d294f479b485100
aaymenb <aymen1996ben@gmail.com>
2026-04-03 11:34:57 +0200

    don

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """
-This module provides a Cache class for basic Redis operations.
+This module expands the Cache class to handle data retrieval
+and type conversion from Redis.
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -21,9 +22,34 @@ class Cache:
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
-        Generates a random key, stores the input data in Redis 
-        using that key, and returns the key.
+        Generates a random key, stores the input data in Redis,
+        and returns the key.
         """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float, None]:
+        """
+        Retrieves data from Redis and optionally applies a 
+        conversion function.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Automatically parametrizes Cache.get to return a string.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Automatically parametrizes Cache.get to return an integer.
+        """
+        return self.get(key, fn=int)
